a04de2051fecd57bbfa40382f346b6c21d42a9d0
lehau17 <hau17131203@gmail.com>
2026-01-20 15:00:00 +0700

    feat(auth): add login and register hooks

---
+++ fe/features/auth/auth.hook.ts
@@ -0,0 +1,50 @@
+
+import { useApiError } from "@/lib/use-api-error";
+import { useMutation } from "@tanstack/react-query";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { AuthApi } from "./auth.api";
+import { TLoginRequest, TRegisterRequest } from "./auth.types";
+
+export const useLogin = () => {
+  const router = useRouter();
+  const { getErrorMessage } = useApiError();
+
+  return useMutation({
+    mutationKey: ["login"],
+
+    mutationFn: (payload: TLoginRequest) => AuthApi.login(payload),
+
+    onSuccess: (data) => {
+      Cookies.set("accessToken", data.tokens.accessToken)
+      Cookies.set("refreshToken", data.tokens.refreshToken)
+      router.push("/");
+    },
+
+    onError: (error: unknown) => {
+      const message = getErrorMessage(error);
+      toast.error(message);
+    },
+  });
+};
+
+export const useRegister = () => {
+  const router = useRouter();
+  const { getErrorMessage } = useApiError();
+
+  return useMutation({
+    mutationKey: ["register"],
+
+    mutationFn: (payload: TRegisterRequest) => AuthApi.register(payload),
+
+    onSuccess: () => {
+      router.push("/login");
+    },
+
+    onError: (error: unknown) => {
+      const message = getErrorMessage(error);
+      toast.error(message);
+    },
+  });
+};
