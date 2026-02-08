06106c2f6391d44d3b5159d58e652382f1401104
lehau17 <lehau171203@gmail.com>
2026-02-08 16:50:45 +0700

    fix(be): update login handler response and object storage factory injection

--- be/apps/main-api/src/modules/identity/application/command/login.handler.ts
+++ be/apps/main-api/src/modules/identity/application/command/login.handler.ts
@@ -9,30 +9,30 @@ import { IUserRepository, USER_REPOSITORY } from '../../domain/port/user.reposit
 import { LoginUserCommand } from './login.command';
 
 @CommandHandler(LoginUserCommand)
-export class LoginUserHandler implements ICommandHandler<LoginUserCommand>{
+export class LoginUserHandler implements ICommandHandler<LoginUserCommand> {
   constructor(
-        @Inject(USER_REPOSITORY) private readonly userRepo: IUserRepository,
+    @Inject(USER_REPOSITORY) private readonly userRepo: IUserRepository,
     @Inject(PASSWORD_HASHER) private readonly passwordHaser: IPasswordHaser,
-        @Inject(TOKEN_SERVICE) private readonly tokenService : ITokenService
-  ){}
- async execute(command: LoginUserCommand): Promise<any> {
-   const foundUser = await this.userRepo.findByEmail(command.email)
-   if (!foundUser) {
-     throw new BadRequestException(ERROR_CODE.USER_NOT_FOUND)
-   }
-   const isMatched = await this.passwordHaser.compare(command.password, foundUser.password)
-   if (!isMatched) {
-     throw new BadRequestException(ERROR_CODE.INVALID_CREDENTIALS)
-   }
-   const tokens =await this.tokenService.generateTokens({ role: "", user_id: foundUser.id })
-   return {
-     user: {
-       id: foundUser.id,
-       email: foundUser.email,
-       fullname : foundUser.fullname
-     },
-     tokens
-   }
+    @Inject(TOKEN_SERVICE) private readonly tokenService: ITokenService
+  ) { }
+  async execute(command: LoginUserCommand): Promise<any> {
+    const foundUser = await this.userRepo.findByEmail(command.email)
+    if (!foundUser) {
+      throw new BadRequestException(ERROR_CODE.USER_NOT_FOUND)
+    }
+    const isMatched = await this.passwordHaser.compare(command.password, foundUser.password)
+    if (!isMatched) {
+      throw new BadRequestException(ERROR_CODE.INVALID_CREDENTIALS)
+    }
+    const tokens = await this.tokenService.generateTokens({ role: "", user_id: foundUser.id })
+    return {
+      user: {
+        id: foundUser.id,
+        email: foundUser.email,
+        fullname: foundUser.fullname
+      },
+      tokens
+    }
 
   }
 
